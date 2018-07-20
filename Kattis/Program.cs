dc950a91a923f8cc489fca7aa2bc78a6e1864bb6
Heisenberg <ari.muayad@gmail.com>
2018-07-20 10:41:20 +0300

    myFirstGit

---
+++ Kattis/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kattis
+
+{
+
+    class Program
+    {
+       static int countDigits(string num)
+        {
+            int i = 0;
+            int sum = 0;
+            while(i<num.Length)
+            {
+                sum += int.Parse(num[i].ToString());
+                i++;
+            }
+
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+
+            int l = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            int x = int.Parse(Console.ReadLine());
+            int low = 0, high = 0;
+            bool s = false;
+            int checker = 0;
+            for(int i = l;i<=d;i++)
+            {
+                if(countDigits(i.ToString())==x&&s==false)
+                {
+                    low = i;
+                    s = true;
+                }
+                if (countDigits(i.ToString()) == x )
+                {
+                    high = i;
+                }
+
+            }
+
+            Console.WriteLine(low);
+            Console.WriteLine(high);
+
+
+        }
+
+    }
+ 
+   }
